a7092c5d77fd7eae57a653cbc12f17bdd0a4184c
ual-laptop <ua-laptop@dhcp-10-134-222-39.uawifi.arizona.edu>
2015-02-09 15:59:03 -0700

    extracting @hackarizona keywords

---
+++ credentials.js
@@ -0,0 +1,8 @@
+var credentials = {
+    consumer_key: 'TKvQR02uMl3QjyiIsydZnhi9o',
+    consumer_secret: 'YaByggIumWS0zLjSKYlendRuVgXGtfAQs2HxVK5R6dx5ghaufM',
+    access_token_key: '1090062254-08z8qyAdDrXUPRyb8lKhdNVOrNKZYfKz6VjCgLp',
+    access_token_secret: '6SXv6a1W7UBXM5a42yn5eT5w2ak40Tmfw4EFRHIq6m82G'
+};
+
+module.exports = credentials;
